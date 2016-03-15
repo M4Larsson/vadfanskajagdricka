7774e125f13dac686958da8c242bfa1f65a0b2d8
Hejhej <mscs_@hotmail.com>
2016-03-15 14:29:41 +0100

    nu skrivs de ut mer

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
 	})
 		$.ajax({
 		method: 'GET',
-        url: 'https://karlroos-systemet.p.mashape.com/product?alcohol_to=30&limit=4&price_from=9&price_to=200&tag=7%2C8%2C16%2C19%2C22%2C23%2C24%2C25',
+        url: 'https://karlroos-systemet.p.mashape.com/product?order_by=name&tag=25%2C7%2C8%2C16%2C19%2C22%2C23%2C24%2C',
         headers: {'X-Mashape-Key': 'OZ9i1HXl2Hmshk5RuUK0N983D9GXp1MsAFnjsnpdlRfMKb7V6F', 'Accept': 'application/json'},
 		
 	}).done(function(data){
@@ -14,12 +14,10 @@ $(document).ready(function(){
 		
 		AllData = data;
 		
-		//hämtar ut ett specifikt objekt...
-		console.log(data[3]);
-		
+
 		var x = document.getElementById("drink")
-		var num = Math.floor((Math.random() * 4) + 1);
-		x.innerHTML = "Drick lite: " + data[num].name + " till det";
-	
+		var num = Math.floor((Math.random() * 49) + 1);
+		x.innerHTML = "Drick lite: " + data[num].name + " till det. Den kostar bara " + data[num].price + "kr på systembolaget, och är ifrån " + data[num].country.name + ". FAN VAD GÖTTT";
+		
 	});
 
